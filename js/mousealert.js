f7de6dce95a1175e8b1a0bbff34fe679e24bea1d
Lydiayaya <2103207398@qq.com>
2021-06-16 22:21:23 +0800

    update

---
+++ js/mousealert.js
@@ -0,0 +1,74 @@
+//点击函数
+
+//汽车输运南货场的点击事件
+let TableAlert = document.getElementsByClassName('TableAlert')[0]
+
+let South = document.getElementsByClassName("south")[0]
+
+
+
+//为弹窗绑定点击弹出函数
+function SouthShow() {
+    TableAlert.style.display = "block"
+}
+function SouthHidden() {
+    TableAlert.style.display = "none"
+}
+//汽车输运占比弹出框
+let TableFlow = document.getElementsByClassName("TableFlow")[0]
+function RateShow() {
+    TableFlow.style.display = "block"
+}
+function RateHidden() {
+    TableFlow.style.display = "none"
+}
+let mapalert =document.getElementsByClassName("mapalert")[0]
+function mapalertShow() {
+    mapalert.style.display = "block"
+}
+function mapalertHidden() {
+    mapalert.style.display = "none"
+}
+//港存动态发运方式详情
+let FuncAlert=document.getElementsByClassName("FuncAlert")[0]
+
+function FuncAlertShow() {
+    FuncAlert.style.display = "block"
+}
+function FuncAlertHidden(){
+    FuncAlert.style.display = "none"
+}
+let FuncAlert2=document.getElementsByClassName("FuncAlert2")[0]
+
+function FuncAlert2Show() {
+    FuncAlert2.style.display = "block"
+}
+function FuncAlert2Hidden(){
+    FuncAlert2.style.display = "none"
+}
+//泊位作业船舶情况
+let openalert=document.getElementsByClassName("openalert")[0]
+
+function openalertShow() {
+    openalert.style.display = "block"
+}
+function openalertHidden(){
+    openalert.style.display = "none"
+}
+//堆场KPI公司数据弹出
+let board=document.getElementsByClassName("board")[0]
+
+function boardtShow() {
+    board.style.display = "block"
+}
+function boardHidden(){
+    board.style.display = "none"
+}
+let Kpialert=document.getElementsByClassName("Kpialert")[0]
+function KpialertShow() {
+    Kpialert.style.display = "block"
+}
+function KpialertHidden(){
+    Kpialert.style.display = "none"
+}
+//指挥中心的运输量弹窗
---
+++ js/page.js
@@ -0,0 +1,33 @@
+//实现页面之间的跳转
+//指挥中心
+function Jumpcenter(){
+    window.location.href="monitorcentertanc.html"; 
+}
+//泊位KpI
+function JumpBowei(){
+    window.location.href="BKPIcur.html"; 
+}
+//泊位作业
+function JumpBoweiWork(){
+    window.location.href="BWopen0408.html"; 
+}
+//火车输运
+function JumpTrain(){
+    window.location.href="HCcenter0408.html"; 
+}
+//汽车输运
+function Jumpcarshuyun(){
+    window.location.href="carshuyun.html"; 
+}
+//港存动态
+function JumpGC(){
+    window.location.href="GCapplyfunction.html"; 
+}
+//业务运行
+function JumpBusiness(){
+    window.location.href="business.html"; 
+}
+//堆场KPI
+function JumpDuichang(){
+    window.location.href="DCangKPI.html"; 
+}
